769f91e3b5fd5b700482579e1af2ff116ade9c1f
alsiberij <alsiberij@gmail.com>
2020-11-24 21:39:14 +0300

    constructMenu() implemented

--- src/root/PlotFrame.java
+++ src/root/PlotFrame.java
@@ -4,6 +4,8 @@ import javax.swing.*;
 import javax.swing.event.MenuEvent;
 import javax.swing.event.MenuListener;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.io.File;
 
 public class PlotFrame extends JFrame {
 
@@ -44,6 +46,52 @@ public class PlotFrame extends JFrame {
         setLocation((kit.getScreenSize().width - WIDTH) / 2, (kit.getScreenSize().height - HEIGHT) / 2);
         setExtendedState(MAXIMIZED_BOTH);
 
+        constructMenu();
         //TODO
+    }
+
+    void constructMenu() {
+        JMenuBar menuBar = new JMenuBar();
+        setJMenuBar(menuBar);
+        JMenu fileMenu = new JMenu("Файл");
+        menuBar.add(fileMenu);
+
+        Action openFileAction = new AbstractAction("Открыть файл") {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (fileChooser == null) {
+                    fileChooser = new JFileChooser();
+                    fileChooser.setCurrentDirectory(new File("."));
+                }
+                if (fileChooser.showOpenDialog(PlotFrame.this) == JFileChooser.APPROVE_OPTION) {
+                    //TODO openFile(fileChooser.getSelectedFile());
+                }
+            }
+        };
+        fileMenu.add(openFileAction);
+
+        JMenu plotMenu = new JMenu("График");
+        menuBar.add(plotMenu);
+
+        Action showAxisAction = new AbstractAction("Показать оси координат") {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                display.setShowAxis(showAxisMI.isSelected());
+            }
+        };
+        showAxisMI = new JCheckBoxMenuItem(showAxisAction);
+        plotMenu.add(showAxisMI);
+        showAxisMI.setSelected(true);
+
+        Action showMarkersAction = new AbstractAction("Показать маркеры точек") {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                display.setShowMarkers(showMarkersMI.isSelected());
+            }
+        };
+        showMarkersMI = new JCheckBoxMenuItem(showMarkersAction);
+        plotMenu.add(showMarkersMI);
+        showMarkersMI.setSelected(false);
+        plotMenu.addMenuListener(new PlotMenuListener());
     }
 }

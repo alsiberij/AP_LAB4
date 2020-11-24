d0af0cc7d148fcfb95978b97377b6293e6219e2e
alsiberij <alsiberij@gmail.com>
2020-11-24 22:19:02 +0300

    main() is configured

--- src/root/MainApplication.java
+++ src/root/MainApplication.java
@@ -1,7 +1,11 @@
 package root;
 
+import javax.swing.*;
+
 public class MainApplication {
     public static void main(String[] args) {
-
+        PlotFrame frame = new PlotFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
     }
 }

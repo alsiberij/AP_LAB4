8e31a031f9f2865d8b02d91fec424ab59a8098c0
alsiberij <alsiberij@gmail.com>
2020-11-24 21:20:09 +0300

    Inner fields added

--- src/root/PlotFrame.java
+++ src/root/PlotFrame.java
@@ -4,4 +4,17 @@ import javax.swing.*;
 
 public class PlotFrame extends JFrame {
 
+    private static final int WIDTH = 1280;
+    private static final int HEIGHT = 720;
+
+    private JFileChooser fileChooser = null;
+
+    private JCheckBoxMenuItem showAxisMI;
+    private JCheckBoxMenuItem showMarkersMI;
+
+    private GraphicsDisplay display = new GraphicsDisplay();
+
+    private boolean fileLoaded = false;
+
+    
 }

cc2fc96bdc5ad6356f7a4831f5535c95007cf60e
alsiberij <alsiberij@gmail.com>
2020-11-24 17:38:53 +0300

    Inner fields added

--- src/root/GraphicsDisplay.java
+++ src/root/GraphicsDisplay.java
@@ -1,7 +1,27 @@
 package root;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class GraphicsDisplay extends JPanel {
 
+    private Double[][] graphicsData;
+
+    private boolean showAxis = true;
+    private boolean showMarkers = true;
+
+    private double xMin;
+    private double xMax;
+    private double yMin;
+    private double yMax;
+
+    private double scale;
+
+    private BasicStroke graphicsStroke;
+    private BasicStroke axisStroke;
+    private BasicStroke markerStroke;
+
+    private Font axisFont;
+
+    
 }
